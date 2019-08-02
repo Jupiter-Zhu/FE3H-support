2acc2cd6e86d45992704b1160f93c15530c75f17
Jupiter Zhu <jupiter.ju.91@gmail.com>
2019-08-01 18:00:56 -0700

    alpha

---
+++ test.py
@@ -0,0 +1,10 @@
+from sty import fg,bg,rs
+y=bg.red+fg.black
+x=bg.rs+fg.rs
+print(y+"Edlegard"+x)
+
+print(bg.blue+fg.white+"Dimitri"+bg.rs+fg.rs)
+
+print(bg.yellow+fg.black+"Claude"+bg.rs+fg.rs)
+
+print(bg.da_white+fg.black+"Rhea"+bg.rs+fg.rs)
